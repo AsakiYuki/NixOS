c96ecf638807fed6f5c9601b90506c17572ca9eb
Asaki Yuki <vantrong2007vn@gmail.com>
2026-03-11 14:43:04 +0700

    idk2

--- flake.nix
+++ flake.nix
@@ -1,40 +1,40 @@
-    {
-        description = "Asa's Nix WSL configuration"; 
+{
+    description = "Asa's Nix configuration (Server & WSL)";
 
-        inputs = {
-            # Default packages manager
-            nixpkgs.url = github:NixOS/nixpkgs/nixos-25.11;
-            home-manager.url = github:nix-community/home-manager/release-25.11;
-
-            # WSL
-            nixos-wsl.url = "github:nix-community/NixOS-WSL";
-            nixos-wsl.inputs.nixpkgs.follows = "nixpkgs";
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+        
+        home-manager = {
+            url = "github:nix-community/home-manager/release-25.11";
+            inputs.nixpkgs.follows = "nixpkgs";
         };
 
-        outputs = inputs@{ self, nixpkgs, home-manager, nixos-wsl, ... }: {
-            nixosConfigurations = {
-                server = nixpkgs.lib.nixosSystem {
-                    system = "x86_64-linux";
-                    specialArgs = {
-                        inherit inputs home-manager;
-                    };
-                    modules = [
-                        home-manager.nixosModules.default
-                        ./devices/server/configuration.nix
-                    ];
-                };
+        nixos-wsl = {
+            url = "github:nix-community/NixOS-WSL";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+    };
 
-                wsl = nixpkgs.lib.nixosSystem {
-                    system = "x86_64-linux";
-                    specialArgs = {
-                        inherit inputs nixos-wsl home-manager;
-                    };
-                    modules = [
-                        nixos-wsl.nixosModules.default
-                        home-manager.nixosModules.default
-                        ./devices/wsl/configuration.nix
-                    ];
-                };
+    outputs = inputs@{ self, nixpkgs, ... }: {
+        nixosConfigurations = {
+            server = nixpkgs.lib.nixosSystem {
+                system = "x86_64-linux";
+                specialArgs = { inherit inputs; };
+                modules = [
+                    inputs.home-manager.nixosModules.default
+                    ./devices/server/configuration.nix
+                ];
+            };
+
+            wsl = nixpkgs.lib.nixosSystem {
+                system = "x86_64-linux";
+                specialArgs = { inherit inputs; };
+                modules = [
+                    inputs.nixos-wsl.nixosModules.default
+                    inputs.home-manager.nixosModules.default
+                    ./devices/wsl/configuration.nix
+                ];
             };
         };
-    }
+    };
+}
