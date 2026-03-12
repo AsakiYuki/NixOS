c1ec931d417a2be47fd2f7b00817476fed8c1167
Asaki Yuki <vantrong2007vn@gmail.com>
2026-03-12 10:21:41 +0700

    change first file rule group from static to system

--- devices/server/modules/systemd/tmpfiles.nix
+++ devices/server/modules/systemd/tmpfiles.nix
@@ -2,7 +2,7 @@ let
     GLOBAL = import ../../_global.nix;
 in {
     rules = [
-        "d ${GLOBAL.SERVER.PUBLIC_FOLDERS.PATH} 0755 asakiyuki static - -"
+        "d ${GLOBAL.SERVER.PUBLIC_FOLDERS.PATH} 0755 asakiyuki system - -"
         "L+ /home/asakiyuki/PUBLIC - - - - ${GLOBAL.SERVER.PUBLIC_FOLDERS.PATH}"
     ];
 }
