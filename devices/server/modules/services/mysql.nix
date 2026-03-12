bcc4c93904e8b46bd74171caa0b27e2a8b9a6b05
Asaki Yuki <vantrong2007vn@gmail.com>
2026-03-12 10:50:32 +0700

    idk

--- devices/server/modules/services/mysql.nix
+++ devices/server/modules/services/mysql.nix
@@ -1,8 +1,23 @@
 { pkgs, ... }: let 
     GLOBAL = import ../../_global.nix;
 in {
-    enable = true;
+    enable = true; 
     package = pkgs.mariadb;
+    settings = {
+        mysqld = {
+            port = GLOBAL.SQL_SERVER_PORT;
+            bind-address = "0.0.0.0";
+        };
+    };
+
+    ensureDatabases = [ "main_db" ];
+    ensureUsers = [
+        {
+            name = "admin";
+            ensurePermissions = { "*.*" = "ALL PRIVILEGES"; };
+        }
+    ];
+
     replication = {
         masterPort = GLOBAL.SQL_SERVER_PORT;
     };
