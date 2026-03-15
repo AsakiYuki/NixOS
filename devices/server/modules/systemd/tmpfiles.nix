b6c5a07586d1c52cd39cac831a6fb10ee0f5fd8c
Asaki Yuki <vantrong2007vn@gmail.com>
2026-03-15 14:49:42 +0700

    idk

--- devices/server/modules/systemd/tmpfiles.nix
+++ devices/server/modules/systemd/tmpfiles.nix
@@ -5,5 +5,9 @@ in {
         "d ${GLOBAL.SERVER.PUBLIC_FOLDERS.PATH} 2775 asakiyuki public - -"
         "L+ /home/asakiyuki/PUBLIC - - - - ${GLOBAL.SERVER.PUBLIC_FOLDERS.PATH}"
         "L+ /home/junko/PUBLIC - - - - ${GLOBAL.SERVER.PUBLIC_FOLDERS.PATH}"
+
+        "d /home/MEDIA_FILES 2775 asakiyuki public - -"
+        "L+ /home/asakiyuki/MEDIA_FILES - - - - /home/MEDIA_FILES"
+        "L+ /home/junko/MEDIA_FILES - - - - /home/MEDIA_FILES"
     ];
 }
