eeaac50a3f565268baaff9f5ae0cc127d287aac1
Asaki Yuki <vantrong2007vn@gmail.com>
2026-03-16 15:25:39 +0700

    fix lyrics services cannot start

--- devices/server/modules/systemd/services/lyrics.nix
+++ devices/server/modules/systemd/services/lyrics.nix
@@ -1,13 +1,13 @@
 { pkgs }: let 
-    import GLOBAL = import ../../../_global.nix;
+    GLOBAL = import ../../../_global.nix;
 in {
     enable = true;
     description = "A self-host musixmatch lyrics service";
     wantedBy = [ "multi-user.target" ];
     after = [ "network.target" ];
     serviceConfig = {
-        ExecStart = "npm run start";
-        WorkingDirectory = "/home/asakiyuki/host/musixmatch-db";
+        ExecStart = "${pkgs.nodejs}/bin/npm run start";
+        WorkingDirectory = "/home/asakiyuki/HOST/musixmatch-db";
         Restart = "always";
         User = "asakiyuki";
     };
