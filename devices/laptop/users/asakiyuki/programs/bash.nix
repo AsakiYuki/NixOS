4abb96f1fc0b719145930e89e2b105b78946b71f
Asaki Yuki <vantrong2007vn@gmail.com>
2026-03-18 11:17:57 +0700

    change quick build system alias from wsl to laptop

--- devices/laptop/users/asakiyuki/programs/bash.nix
+++ devices/laptop/users/asakiyuki/programs/bash.nix
@@ -4,7 +4,7 @@ in {
     programs.bash =  {
         enable = true;
         shellAliases = defaultBash.shellAliases // {
-            nrs = "sudo nixos-rebuild switch --flake /etc/nixos#wsl";
+            nrs = "sudo nixos-rebuild switch --flake /etc/nixos#laptop";
         };
 
         shellOptions = defaultBash.shellOptions;
