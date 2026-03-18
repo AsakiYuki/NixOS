7739612a58d1aa4635225dc88e433d98b7b34ebd
Asaki Yuki <vantrong2007vn@gmail.com>
2026-03-18 11:00:16 +0700

    home manager

--- devices/laptop/configuration.nix
+++ devices/laptop/configuration.nix
@@ -4,22 +4,48 @@
         ./hardware-configuration.nix
     ];
 
+    # BẮT BUỘC CHO MÁY MỚI: Sử dụng Kernel mới nhất để nhận driver Audio/Mic của AMD Ryzen AI
+    boot.kernelPackages = pkgs.linuxPackages_latest;
+
     boot.loader.systemd-boot.enable = true;
     boot.loader.efi.canTouchEfiVariables = true;
 
     networking.hostName = "nixos";
     networking.networkmanager.enable = true;
 
+    boot.kernelParams = [
+        "snd_hda_intel.model=lenovo" 
+        "snd_acp_config.dmic_acpi_check=1"
+        "snd_acp_config.dmic_detect=1"
+        "snd_sof_amd_acp63_enable=1"
+    ];
+
+    boot.extraModprobeConfig = ''
+        options snd-hda-intel dmic_detect=0
+    '';
+
+    # Cấp quyền Realtime cho Pipewire hoạt động trơn tru (Bắt buộc)
+    security.rtkit.enable = true;
+    services.flatpak.enable = true;
+
+    # Cấu hình lại Pipewire đầy đủ
     services.pipewire = {
         enable = true;
+        alsa.enable = true;        # Hỗ trợ các app dùng ALSA
+        alsa.support32Bit = true;  
         pulse.enable = true;
+        wireplumber.enable = true; # Trình quản lý session âm thanh (rất quan trọng)
     };
+
+    # Đảm bảo firmware độc quyền của AMD được tải đầy đủ
+    hardware.enableRedistributableFirmware = true;
+    hardware.firmware = [ pkgs.sof-firmware ];
 
     users.users.asakiyuki = {
         isNormalUser = true;
-        extraGroups = [ "wheel" ];
+        extraGroups = [ "wheel" "audio" "video" ]; # Thêm user vào group audio
         packages = with pkgs; [
-          tree
+            tree
         ];
     };
 
@@ -33,10 +59,16 @@
         vscode
         obs-studio
         vlc
+        pavucontrol 
+        alsa-utils 
+        alsa-ucm-conf
+
+        (pkgs.catppuccin-kde.override {
+            flavour = [ "mocha" ];
+        })
     ];
 
     nixpkgs.config.allowUnfree = true;
-
 
     services = {
         desktopManager.plasma6.enable = true;
---
+++ devices/laptop/users/users.nix
@@ -0,0 +1,18 @@
+{ inputs, pkgs, lib, ... }: {
+    users.users.asakiyuki = {
+        isNormalUser = true;
+        shell = pkgs.bash;
+        extraGroups = [ "public" "wheel" "docker" ];
+    };
+
+    home-manager = {
+        useUserPackages = true;
+        useGlobalPkgs = true;
+        backupFileExtension = "bak";
+
+        users.asakiyuki = {
+            _module.args = { inherit inputs pkgs; };
+            imports = [ ./asakiyuki/configuration.nix ];
+        };
+    };
+}
