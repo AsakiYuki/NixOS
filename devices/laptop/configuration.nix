36102162794ddac8aeeac532db923b30b9cc26f5
Asaki Yuki <vantrong2007vn@gmail.com>
2026-03-18 11:42:59 +0700

    add fcitx5 unikey method

--- devices/laptop/configuration.nix
+++ devices/laptop/configuration.nix
@@ -21,5 +21,14 @@
     nix.settings.experimental-features = [ "nix-command" "flakes" ];
     time.timeZone = "Asia/Ho_Chi_Minh";
 
+    i18n.inputMethod = {
+        enable = true;
+        type = "fcitx5";
+
+        fcitx5.addons = with pkgs; [
+            fcitx5-bamboo
+        ];
+    };
+
     system.stateVersion = "25.11";
 }
