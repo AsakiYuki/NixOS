acff30194575d0c0d7a0d7667639570bf26620ef
Asaki Yuki <vantrong2007vn@gmail.com>
2026-03-19 14:03:47 +0700

    dummy

--- devices/laptop/users/asakiyuki/modules/files/_files.nix
+++ devices/laptop/users/asakiyuki/modules/files/_files.nix
@@ -1,5 +1,5 @@
 { pkgs, ... }: {
     home.file = {
-        ".local/share/kio/servicemenus".source = ;
+        ".local/share/kio/servicemenus/open-with-code.desktop".source = ./sources/open-with-code.desktop;
     };
 }
