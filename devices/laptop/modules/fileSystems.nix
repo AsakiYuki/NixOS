d951d470add7d5696d9e67bf52fc4c4449008a20
Asaki Yuki <vantrong2007vn@gmail.com>
2026-03-20 00:03:09 +0700

    .

--- devices/laptop/modules/fileSystems.nix
+++ devices/laptop/modules/fileSystems.nix
@@ -1,11 +1,13 @@
 { ... }: {
-  fileSystems."/mnt/waydroid" = {
-        device = "/home/asakiyuki/.local/share/waydroid/data/media/0";
-        fsType = "fuse.bindfs";
-        options = [
-            "mirror=1000"
-            "xattr-none"
-            "chmod-ignore"
-        ];
+    fileSystems = {
+        "/mnt/waydroid" = {
+            device = "/home/asakiyuki/.local/share/waydroid/data/media/0";
+            fsType = "fuse.bindfs";
+            options = [
+                "mirror=1000"
+                "xattr-none"
+                "chmod-ignore"
+            ];
+        };
     };
 }
