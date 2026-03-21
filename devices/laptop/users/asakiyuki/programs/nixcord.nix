df6d09faeaf95bc9c992413941aa4bf82857825f
Asaki Yuki <vantrong2007vn@gmail.com>
2026-03-21 22:17:08 +0700

    sneaky

--- devices/laptop/users/asakiyuki/programs/nixcord.nix
+++ devices/laptop/users/asakiyuki/programs/nixcord.nix
@@ -1,6 +1,21 @@
 { ... }: {
     programs.nixcord = {
         enable = true;
-        discord.vencord.enable = true; 
+        discord = {
+            enable = true;
+            vencord.enable = true; 
+        };
+
+        config = {
+            enableReactDevtools = true;
+            themeLinks = [ "https://catppuccin.github.io/discord/dist/catppuccin-mocha-blue.theme.css" ];
+
+            plugins = {
+                fakeNitro = {
+                    enable = true;
+                    enableEmojiBypass = false;
+                };
+            };
+        };
     };
 }
