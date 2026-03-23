10e7e7e34e1cdce72621ab1980e31e73d1eae82e
Asaki Yuki <vantrong2007vn@gmail.com>
2026-03-23 18:37:57 +0700

    change some packages from system to user

--- devices/laptop/modules/packages.nix
+++ devices/laptop/modules/packages.nix
@@ -1,66 +1,26 @@
-{ inputs, pkgs, unstable, ... }: {
+{ inputs, pkgs, ... }: {
     environment.systemPackages = (with pkgs; [
-        gcc
-        cmake
-        gdb
-        pkg-config
-
         vim
         wget
         git
-        alsa-utils 
+        alsa-utils
         alsa-ucm-conf
-        nodejs
-        bun
+        
         mesa
         dav1d
-        ffmpeg-full
         libva
         libva-utils
         libva-vdpau-driver
         docker-compose
         ntfs3g
         btop
-        neovide
-        sass
-        ruby
-        rPackages.gems
         bindfs
         tree
         superfile
 
-        easyeffects
-        protonup-qt
-        obs-studio
-        pavucontrol
-        antigravity
-        chromium
-        cider-2
-        gimp
-        waydroid-helper
-        osu-lazer
-        waydroid-nftables
-        qbittorrent
-        vlc
-
-        wine
-        wine64
-        winetricks
-        protontricks
-        php
-
         fcitx5-material-color
-        kdePackages.kde-gtk-config
-    ]) ++ (with unstable; [
-        proton-pass
-        vscode
-        proton-authenticator
-        prismlauncher
-        vesktop
-        winboat
-        freerdp
-        lutris
     ]) ++ (with pkgs.kdePackages;[
+        kde-gtk-config
         qtdeclarative
         qt5compat
         kirigami
@@ -68,17 +28,5 @@
         qtbase
         plasma5support
         qttools
-    ]) ++ (with inputs; [
-        hytale-launcher.packages.${pkgs.stdenv.hostPlatform.system}.default
-    ]) ++ [
-        (pkgs.catppuccin-kde.override {
-            flavour = [ "mocha" ];
-            accents = [ "sapphire" ];
-        })
-
-        (pkgs.catppuccin-gtk.override {
-            variant = "mocha";
-            accents = [ "sapphire" ];
-        })
-    ];
+    ]);
 }   
--- devices/laptop/users/asakiyuki/packages/_packages.nix
+++ devices/laptop/users/asakiyuki/packages/_packages.nix
@@ -1,3 +1,61 @@
-{ ... }: {
+{ inputs, pkgs, unstable ... }: let 
+    PKGS = with pkgs; [
+        nodejs
+        bun
+        ffmpeg-full
+        neovide
+        sass
+        ruby
+        rPackages.gems
+
+
+        easyeffects
+        protonup-qt
+        obs-studio
+        pavucontrol
+        antigravity
+        chromium
+        cider-2
+        gimp
+        waydroid-helper
+        osu-lazer
+        waydroid-nftables
+        qbittorrent
+        vlc
+
+        wine
+        wine64
+        winetricks
+        protontricks
+        php
+    ];
     
+    UNSTABLE_PKGS = with unstable; [
+        proton-pass
+        vscode
+        proton-authenticator
+        prismlauncher
+        vesktop
+        winboat
+        freerdp
+        lutris
+    ];
+
+    INPUTS_PKGS = with inputs; [
+        hytale-launcher.packages.${pkgs.stdenv.hostPlatform.system}.default
+    ];
+
+    THEMES = [
+        (pkgs.catppuccin-kde.override {
+            flavour = [ "mocha" ];
+            accents = [ "sapphire" ];
+        })
+
+        (pkgs.catppuccin-gtk.override {
+            variant = "mocha";
+            accents = [ "sapphire" ];
+        })
+    ];
+in  {
+    home.packages = PKGS ++ UNSTABLE_PKGS ++ INPUTS_PKGS;
 }
