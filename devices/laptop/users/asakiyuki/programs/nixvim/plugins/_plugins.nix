33bd0619d9cf8dd9ddbc9c577f1f08465f7fd018
Asaki Yuki <vantrong2007vn@gmail.com>
2026-03-24 17:59:41 +0700

    add nvimtree

--- devices/laptop/users/asakiyuki/programs/nixvim/plugins/_plugins.nix
+++ devices/laptop/users/asakiyuki/programs/nixvim/plugins/_plugins.nix
@@ -2,6 +2,7 @@
 {
     programs.nixvim.plugins = {
         neo-tree = import ./neotree.nix;
+        nvim-tree = ;
         web-devicons.enable = true;
         which-key.enable = true;
         treesitter.enable = true;
