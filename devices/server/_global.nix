b77c3f6b22c266f262716cf7bc2aff632c12ea23
Asaki Yuki <vantrong2007vn@gmail.com>
2026-03-25 22:18:48 +0700

    add kvvl to allow login user (server)

--- devices/server/_global.nix
+++ devices/server/_global.nix
@@ -1,6 +1,6 @@
 let
     ADMIN_EMAIL = "vantrong2007vn@gmail.com";
-    ALLOWED_SSH_USERS = [ "asakiyuki" "junko" "hoacaclord" ];
+    ALLOWED_SSH_USERS = [ "asakiyuki" "junko" "hoacaclord" "kvvl" ];
     
     SSH_PORT = 15523;
     SQL_SERVER_PORT = 37241;
