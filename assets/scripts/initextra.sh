a3497bc37352a8cbe1eb2856c235e03e37e2d5b4
Asaki Yuki <vantrong2007vn@gmail.com>
2026-03-25 19:43:16 +0700

    add initextra.sh

---
+++ assets/scripts/initextra.sh
@@ -0,0 +1,19 @@
+function git-commit() {
+    git add .
+    git commit -m "$1"
+}
+
+function git-push()  {
+    git add .
+    git commit -m "$1"
+    git push origin HEAD
+}
+
+function git-pull() {
+    git fetch origin HEAD
+    git pull origin HEAD
+}
+
+function get-hash() {
+    nix hash to-sri --type sha256 $(nix-prefetch-url --unpack "$1")
+}
--- common/programs/bash.nix
+++ common/programs/bash.nix
@@ -14,25 +14,5 @@
         "autocd"
     ];
 
-    initExtra = ''
-        function git-commit() {
-            git add .
-            git commit -m "$1"
-        }
-
-        function git-push()  {
-            git add .
-            git commit -m "$1"
-            git push origin HEAD
-        }
-
-        function git-pull() {
-            git fetch origin HEAD
-            git pull origin HEAD
-        }
-
-        function get-hash() {
-            nix hash to-sri --type sha256 $(nix-prefetch-url --unpack "$1")
-        }
-    '';
+    initExtra = builtins.readFile ../../assets/scripts/initextra.sh;
 }
