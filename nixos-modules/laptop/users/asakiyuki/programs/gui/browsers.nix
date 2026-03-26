fed4e56475fcd018496f3cecee488820bb2ed3f3
Asaki Yuki <vantrong2007vn@gmail.com>
2026-03-26 12:58:52 +0700

    add search engines

--- nixos-modules/laptop/users/asakiyuki/programs/gui/browsers.nix
+++ nixos-modules/laptop/users/asakiyuki/programs/gui/browsers.nix
@@ -13,11 +13,62 @@
                         youtube = {
                             name = "YouTube";
                             definedAliases = [ "@yt" "@youtube" ];
+                            icon = "https://www.youtube.com/s/desktop/83c08b77/img/favicon.ico";
                             urls = [
                                 {
                                     template = "https://www.youtube.com/results";
                                     params = [
                                         { name = "search_query"; value = "{searchTerms}"; }
+                                    ];
+                                }
+                            ];
+                        };
+
+                        google = {
+                            name = "Google";
+                            definedAliases = [ "@g" "@google" ];
+                            icon = "https://www.gstatic.com/images/branding/searchlogo/ico/favicon.ico";
+                            urls = [
+                                {
+                                    template = "https://www.google.com/search";
+                                    params = [
+                                        { name = "q"; value = "{searchTerms}"; }
+                                    ];
+                                }
+                                {
+                                    template = "https://www.google.com/search";
+                                    params = [
+                                        { name = "udm"; value = "2"; }
+                                        { name = "q"; value = "{searchTerms}"; }
+                                    ];
+                                }
+                            ];
+                        };
+
+                        github = {
+                            name = "Github";
+                            definedAliases = [ "@gh" "@github" ];
+                            icon = "https://github.githubassets.com/favicons/favicon-dark.svg";
+                            urls = [
+                                {
+                                    template = "https://github.com/search";
+                                    params = [
+                                        { name = "type"; value = "repositories"; }
+                                        { name = "q"; value = "{searchTerms}"; }
+                                    ];
+                                }
+                            ];
+                        };
+
+                        reddit = {
+                            name = "Reddit";
+                            definedAliases = [ "@rd" "@reddit" ];
+                            icon = "https://www.redditstatic.com/shreddit/assets/favicon/192x192.png";
+                            urls = [
+                                {
+                                    template = "https://www.reddit.com/search";
+                                    params = [
+                                        { name = "q"; value = "{searchTerms}"; }
                                     ];
                                 }
                             ];
@@ -41,6 +92,7 @@
                         mynixos = {
                             name = "My NixOS";
                             definedAliases = [ "@mynixos" ];
+                            icon = "https://mynixos.com/favicon.ico";
                             urls = [
                                 {
                                     template = "https://mynixos.com/search";
