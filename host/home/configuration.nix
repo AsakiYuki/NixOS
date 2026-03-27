8e06e8566bead986abfaabe532780493bb42bc7b
Asaki Yuki <vantrong2007vn@gmail.com>
2026-03-27 20:00:42 +0700

    remove port 3000

--- host/home/configuration.nix
+++ host/home/configuration.nix
@@ -26,8 +26,8 @@
             nginx-proxy-manager.enable = true;
             nginx-proxy-manager.ports = [ 80 18581 443 ];
 
-            ports.allowed.udp = [ 53 3000 34778 ];
-            ports.allowed.tcp = [ 53 3000 ];
+            ports.allowed.udp = [ 53 34778 ];
+            ports.allowed.tcp = [ 53 ];
         };
     };
 
