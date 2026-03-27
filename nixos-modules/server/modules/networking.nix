6aaca3d7b59495ed781ab95f1f9ac61d350973a7
Asaki Yuki <vantrong2007vn@gmail.com>
2026-03-28 01:58:48 +0700

    idk

--- nixos-modules/server/modules/networking.nix
+++ nixos-modules/server/modules/networking.nix
@@ -11,13 +11,12 @@ in {
         interfaces = {
             enp1s0 = {
                 useDHCP = false;
+                
                 ipv4.addresses = [{
                     address = "192.168.1.100";
                     prefixLength = 24;
                 }];
                 
-                ipv6.useDHCP = false;
-                ipv6.acceptRA = false;
                 ipv6.addresses = [{
                     address = "2402:800:62d0:1c26:abcd:1234:5678:9abc";
                     prefixLength = 64;
