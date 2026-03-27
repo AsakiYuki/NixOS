7ce9d21928e414b89b4a4fd8f7d29ddf96226658
Asaki Yuki <vantrong2007vn@gmail.com>
2026-03-27 10:27:05 +0700

    change server/default.nix to server/configuration.nix

--- host/home/configuration.nix
+++ host/home/configuration.nix
@@ -2,7 +2,7 @@
     # Import Modules
     imports = [
         ./hardware-configuration.nix
-        ../../nixos-modules/server/default.nix
+        ../../nixos-modules/server/configuration.nix
     ];
 
     system.stateVersion = "25.11";
