3dce7eb70b32e65ad4aade9985f849712194da1a
Asaki Yuki <vantrong2007vn@gmail.com>
2026-03-27 18:14:55 +0700

    idk

--- host/home/configuration.nix
+++ host/home/configuration.nix
@@ -26,7 +26,7 @@
             nginx-proxy-manager.enable = true;
             nginx-proxy-manager.ports = [ 80 18581 443 ];
 
-            ports.allowed.udp = [ 53 ];
+            ports.allowed.udp = [ 53 3000 ];
             ports.allowed.tcp = [ 53 ];
         };
     };
