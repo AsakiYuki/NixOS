285ea20f220cd9c8c65c1ba44ccc440646904769
Asaki Yuki <vantrong2007vn@gmail.com>
2026-04-04 17:24:46 +0700

    add some nvim plugins

--- common/programs/nixvim/plugins/_plugins.nix
+++ common/programs/nixvim/plugins/_plugins.nix
@@ -1,9 +1,17 @@
-{ ... }:
+{ ... }@inputs:
 {
     programs.nixvim.plugins = {
         nvim-tree = import ./nvimtree.nix;
+        cmp = import ./cmp.nix inputs;
+
         web-devicons.enable = true;
         which-key.enable = true;
         treesitter.enable = true;
+        lualine.enable = true;
+        neoscroll.enable = true;
+        lz-n.enable = true;
+        image.enable = true;
+        nvim-autopairs.enable = true;
+        bufferline.enable = true;
     };
 }
