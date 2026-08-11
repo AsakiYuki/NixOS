3fc7ad293852001b8bedff9e93a145ab74d156f4
Asaki Yuki <vantrong2007vn@gmail.com>
2026-08-12 01:39:10 +0700

    chore: add ge-proton-11-5 to steam compatibility tools

--- flakes/proton/home.nix
+++ flakes/proton/home.nix
@@ -1,3 +1,10 @@
-{...}: {
-  config.home.file = {};
+{pkgs, ...}: let
+  prefix = ".local/share/Steam/compatibilitytools.d";
+in {
+  config.home.file = {
+    "${prefix}/ge-proton-11-5".source = pkgs.fetchzip {
+      url = "https://github.com/GloriousEggroll/proton-ge-custom/releases/download/GE-Proton11-5/GE-Proton11-5-x86_64.tar.gz";
+      hash = "sha256-Sbyi5zXMhPIKSotvL5LEZ2dbDoLpXRcCyuY9TsnBnus=";
+    };
+  };
 }
