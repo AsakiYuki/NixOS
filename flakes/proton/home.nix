9dcd9e83709cc1528cfe8cd1faf3db201618b765
Asaki Yuki <vantrong2007vn@gmail.com>
2026-08-12 01:46:58 +0700

    chore(flake/proton): import options

--- flakes/proton/home.nix
+++ flakes/proton/home.nix
@@ -1,6 +1,10 @@
 {pkgs, ...}: let
   prefix = ".local/share/Steam/compatibilitytools.d";
 in {
+  imports = [
+    ./options.nix
+  ];
+
   config.home.file = {
     "${prefix}/ge-proton-11-5".source = pkgs.fetchzip {
       url = "https://github.com/GloriousEggroll/proton-ge-custom/releases/download/GE-Proton11-5/GE-Proton11-5-x86_64.tar.gz";
