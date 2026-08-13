cedb5ed36336357708f81639ad9a6857763f90a8
Asaki Yuki <vantrong2007vn@gmail.com>
2026-08-13 13:11:43 +0700

    chore: remove ge-proton-11-5

--- flakes/proton/home.nix
+++ flakes/proton/home.nix
@@ -5,10 +5,10 @@ in {
     ./options.nix
   ];
 
-  config.home.file = {
-    "${prefix}/ge-proton-11-5".source = pkgs.fetchzip {
-      url = "https://github.com/GloriousEggroll/proton-ge-custom/releases/download/GE-Proton11-5/GE-Proton11-5-x86_64.tar.gz";
-      hash = "sha256-Sbyi5zXMhPIKSotvL5LEZ2dbDoLpXRcCyuY9TsnBnus=";
-    };
-  };
+  # config.home.file = {
+  #   "${prefix}/ge-proton-11-5".source = pkgs.fetchzip {
+  #     url = "https://github.com/GloriousEggroll/proton-ge-custom/releases/download/GE-Proton11-5/GE-Proton11-5-x86_64.tar.gz";
+  #     hash = "sha256-Sbyi5zXMhPIKSotvL5LEZ2dbDoLpXRcCyuY9TsnBnus=";
+  #   };
+  # };
 }
