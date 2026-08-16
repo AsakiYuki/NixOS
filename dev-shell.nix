540ce9d4eb5133ba794f8009499fc8f124df28bd
Asaki Yuki <vantrong2007vn@gmail.com>
2026-08-17 03:23:17 +0700

    fix: devShells.x86_64-linux.default not found

--- dev-shell.nix
+++ dev-shell.nix
@@ -1,5 +1,7 @@
-{...}: {
-  devShells.x86_64-linux.default = {
+{nixpkgs, ...}: let
+  pkgs = import nixpkgs {system = "x86_64-linux";};
+in {
+  devShells.x86_64-linux.default = pkgs.mkShell {
     shellHook = ''
       echo "Welcome to the devShell!"
     '';
