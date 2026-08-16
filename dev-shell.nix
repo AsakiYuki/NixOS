2d4365522461edeac9793ebca8eb17aeaf263b80
Asaki Yuki <vantrong2007vn@gmail.com>
2026-08-17 03:30:38 +0700

    dev-shell test

--- dev-shell.nix
+++ dev-shell.nix
@@ -1,8 +1,13 @@
-{nixpkgs, ...}: let
-  pkgs = import nixpkgs {system = "x86_64-linux";};
+{nixpkgs, ...} @ inputs: let
+  pkgs = import nixpkgs {
+    system = "x86_64-linux";
+    overlays = import ./overlays/inputs-overlays.nix inputs;
+  };
 in {
   devShells.x86_64-linux.default = pkgs.mkShell {
-    buildInput = with pkgs; [hello];
+    buildInputs = with pkgs; [
+      hello
+    ];
 
     shellHook = ''
       echo "Welcome to the devShell!"
