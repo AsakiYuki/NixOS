d3273556e9428d7f0e1eee67c017c6dca5efa166
Asaki Yuki <vantrong2007vn@gmail.com>
2026-08-17 03:25:45 +0700

    chore: remove repl.sh

--- dev-shell.nix
+++ dev-shell.nix
@@ -2,6 +2,8 @@
   pkgs = import nixpkgs {system = "x86_64-linux";};
 in {
   devShells.x86_64-linux.default = pkgs.mkShell {
+    buildInput = with pkgs; [hello];
+
     shellHook = ''
       echo "Welcome to the devShell!"
     '';
