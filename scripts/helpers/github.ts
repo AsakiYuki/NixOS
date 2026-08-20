9c34f65f8ab00a84d5306b1f43191b24b6cf342f
Asaki Yuki <vantrong2007vn@gmail.com>
2026-08-20 12:00:26 +0700

    chore: GH_TOKEN

--- scripts/helpers/github.ts
+++ scripts/helpers/github.ts
@@ -1,6 +1,5 @@
 const GH_TOKEN = process.env.GH_TOKEN
 const auth: HeadersInit = GH_TOKEN ? { Authorization: `Bearer ${GH_TOKEN}` } : {}
-console.log(auth === undefined)
 
 export interface Asset {
 	name: string
