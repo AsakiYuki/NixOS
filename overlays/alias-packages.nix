29b4b34f289eb4022020ab652d70c7080c980909
Asaki Yuki <vantrong2007vn@gmail.com>
2026-08-24 21:19:56 +0700

    fix(overlays/iris): update iris vendorHash

--- overlays/alias-packages.nix
+++ overlays/alias-packages.nix
@@ -7,7 +7,7 @@
     (final: prev: {
       zen-browser = inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.default;
       iris = inputs.iris.packages.${pkgs.stdenv.hostPlatform.system}.default.overrideAttrs (final: prev: {
-        vendorHash = "sha256-q1szUQkhdKq2VhMuWYYWTahmDxGeVjvHLmjciZu3cBU=";
+        vendorHash = "sha256-huyTWK6ef42KY2zmFIQuFoeR8B8XKHE7OVfFnfefeCU=";
       });
     })
   ];
