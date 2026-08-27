2ca34b95df397108c5b3d427b3a35809b166d4be
Asaki Yuki <vantrong2007vn@gmail.com>
2026-08-27 18:25:56 +0700

    chore(modules/home/themes/gtk): use system catppuccin theme

--- modules/home/theme/scripts.nix
+++ modules/home/theme/scripts.nix
@@ -1,27 +1,30 @@
 {
   pkgs,
   lib,
+  osconfig,
   ...
 }: let
+  cfg = osconfig.device.theme.catppuccin;
+
   catppuccin-gtk = pkgs.catppuccin-gtk.override {
-    variant = "mocha";
-    accents = ["sapphire"];
+    variant = cfg.flavor;
+    accents = [cfg.accent];
     size = "compact";
   };
 in {
   home.activation.copyGtkTheme = lib.hm.dag.entryAfter ["writeBoundary"] ''
     mkdir -p "$HOME/.themes/"
 
-    if [ ! -d "$HOME/.themes/catppuccin-mocha-sapphire-compact" ]; then
-        cp -r "${catppuccin-gtk}/share/themes/catppuccin-mocha-sapphire-compact" "$HOME/.themes/"
+    if [ ! -d "$HOME/.themes/catppuccin-${cfg.flavor}-${cfg.accent}-compact" ]; then
+        cp -r "${catppuccin-gtk}/share/themes/catppuccin-${cfg.flavor}-${cfg.accent}-compact" "$HOME/.themes/"
     fi
 
-    if [ ! -d "$HOME/.themes/catppuccin-mocha-sapphire-compact-hdpi" ]; then
-        cp -r "${catppuccin-gtk}/share/themes/catppuccin-mocha-sapphire-compact-hdpi" "$HOME/.themes/"
+    if [ ! -d "$HOME/.themes/catppuccin-${cfg.flavor}-${cfg.accent}-compact-hdpi" ]; then
+        cp -r "${catppuccin-gtk}/share/themes/catppuccin-${cfg.flavor}-${cfg.accent}-compact-hdpi" "$HOME/.themes/"
     fi
 
-    if [ ! -d "$HOME/.themes/catppuccin-mocha-sapphire-compact-xhdpi" ]; then
-        cp -r "${catppuccin-gtk}/share/themes/catppuccin-mocha-sapphire-compact-xhdpi" "$HOME/.themes/"
+    if [ ! -d "$HOME/.themes/catppuccin-${cfg.flavor}-${cfg.accent}-compact-xhdpi" ]; then
+        cp -r "${catppuccin-gtk}/share/themes/catppuccin-${cfg.flavor}-${cfg.accent}-compact-xhdpi" "$HOME/.themes/"
     fi
   '';
 }
