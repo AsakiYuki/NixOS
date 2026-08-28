19cda47fd0af88f3827d3d4705057f8bffec0b43
Asaki Yuki <vantrong2007vn@gmail.com>
2026-08-28 14:51:49 +0700

    feat(helpers): add hexToDec lib function

--- helpers/default.nix
+++ helpers/default.nix
@@ -2,9 +2,10 @@
   fs = import ./fileSystem.nix;
   mkAuthKeys = import ./mkAuthorizedKeys.nix;
   mkProgOpt = import ./mkProgramOption.nix args;
+  helpers = import ./helpers.nix args;
   attrs = {
     mkUsers = import ./mkUsers.nix args;
   };
   catppuccin = import ./catppuccin.nix;
 in
-  fs // mkAuthKeys // mkProgOpt // attrs // {inherit catppuccin;}
+  fs // mkAuthKeys // mkProgOpt // attrs // helpers // {inherit catppuccin;}
