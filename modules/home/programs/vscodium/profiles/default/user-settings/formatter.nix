e8bd5cb6df384e2f735d5b021617a1fccf8cbef7
Asaki Yuki <vantrong2007vn@gmail.com>
2026-08-29 18:11:05 +0700

    feat(home/vscodium): add formatter for each language

--- modules/home/programs/vscodium/profiles/default/user-settings/formatter.nix
+++ modules/home/programs/vscodium/profiles/default/user-settings/formatter.nix
@@ -1,7 +1,19 @@
-{...}: {
-    programs.vscodium.profiles.default.userSettings = {
+{lib, ...}: let 
+    formatter-per-lang = {
+        cpp = "ms-vscode.cpptools";
+        xml = "rogalmic.vscode-xml-complete";
+        nix = "jnoortheen.nix-ide";
+        qml = "theqtcompany.qt-qml";
+        php = "bmewburn.vscode-intelephense-client";
+        shellscript = "mads-hartmann.bash-ide-vscode";
+    };
+in  {
+    programs.vscodium.profiles.default.userSettings = (lib.mergeAttrs {
         # Formatter
         "editor.defaultFormatter" = "esbenp.prettier-vscode";
         "notebook.defaultFormatter" = "esbenp.prettier-vscode";
-    };
+    } (lib.mapAttrs' (name: value: {
+        name = "[${name}]";
+        value."editor.defaultFormatter" = value;
+    }) formatter-per-lang));
 }
