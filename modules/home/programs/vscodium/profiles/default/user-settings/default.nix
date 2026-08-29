b31d02306d40f2e29bca45c0d5091a351eeb9902
Asaki Yuki <vantrong2007vn@gmail.com>
2026-08-29 17:23:26 +0700

    feat(home/vscodium): add fonts to user settings

--- modules/home/programs/vscodium/profiles/default/user-settings/default.nix
+++ modules/home/programs/vscodium/profiles/default/user-settings/default.nix
@@ -3,4 +3,10 @@
         ./vscord.nix
         ./catppuccin.nix
     ];
+
+    programs.vscodium.profiles.default.userSettings = {
+        "editor.fontSize" = 12;
+        "editor.fontFamily" = "Tahoma, \"SauceCodePro Nerd Font\"";
+        "terminal.integrated.fontFamily" = "\"SauceCodePro Nerd Font\"";
+    };
 }
