cdb600ef62ddf252fd835c4afe4a7339d099cedd
Asaki Yuki <vantrong2007vn@gmail.com>
2026-08-30 09:59:51 +0700

    feat(assets/packages.json): add extensions metadata

--- flakes/overlays/vscode-extensions/default.nix
+++ flakes/overlays/vscode-extensions/default.nix
@@ -1,14 +1,16 @@
 {pkgs}: let
   inherit (pkgs) vscode-utils lib;
   inherit (vscode-utils) buildVscodeMarketplaceExtension;
+
+  extensions = (lib.importJSON ../../../assets/packages.json).vscode-extensions;
 in {
   jbro.vscode-default-keybindings = buildVscodeMarketplaceExtension {
-    mktplcRef = {
-      name = "vscode-default-keybindings";
-      publisher = "jbro";
-      version = "0.2.61";
-      hash = "sha256-O/PvflmD/Sv+4CdAq9UXIiVKiO1lKDH91pWxDeAGgFE=";
-    };
+    mktplcRef =
+      extensions."jbro.vscode-default-keybindings"
+      // {
+        name = "vscode-default-keybindings";
+        publisher = "jbro";
+      };
     meta = {
       description = "VSCode Default Keybindings is a keymap that enables you to load VSCode's default keybindings from another OS. Eg. use Mac keybindings on Windows.";
       downloadPage = "https://marketplace.visualstudio.com/items?itemName=jbro.vscode-default-keybindings";
