df313737eb0a5f8c5eefa8d0758a0b2ce2f551ac
Asaki Yuki <vantrong2007vn@gmail.com>
2026-09-01 21:56:51 +0700

    chore: idk

--- configs/nix-on-droid.nix
+++ configs/nix-on-droid.nix
@@ -4,10 +4,7 @@
   ...
 }: {
   nixOnDroidConfigurations.default = nix-on-droid.lib.nixOnDroidConfiguration {
-    pkgs = import nixpkgs {
-      system = "aarch64-linux";
-      overlays = [nix-on-droid.overlays.default];
-    };
+    pkgs = import nixpkgs {system = "aarch64-linux";};
 
     modules = [../host/android];
   };
