8e71200b9d9c45796476dda0b635f1d7ba6b9b23
Asaki Yuki <vantrong2007vn@gmail.com>
2026-09-01 14:27:40 +0700

    feat(home/sessionVariables): add NIXPKGS_ALLOW_UNFREE

--- modules/home/sessionVariables.nix
+++ modules/home/sessionVariables.nix
@@ -9,8 +9,13 @@
     (builtins.elem pkgs.geode-cli osconfig.environment.systemPackages)
     || (builtins.elem pkgs.geode-cli config.home.packages);
 in {
-  home.sessionVariables = lib.mkIf isGeodeSDKInstalled {
-    GEODE_SDK = "${config.home.homeDirectory}/Documents/Geode";
-    GEODE_TARGET_PLATFORM = "Win64";
-  };
+  home.sessionVariables = lib.mergeAttrsList [
+    (lib.optionalAttrs isGeodeSDKInstalled {
+      GEODE_SDK = "${config.home.homeDirectory}/Documents/Geode";
+      GEODE_TARGET_PLATFORM = "Win64";
+    })
+    (lib.optionalAttrs osconfig.nixpkgs.config.allowUnfree {
+      NIXPKGS_ALLOW_UNFREE = 1;
+    })
+  ];
 }
