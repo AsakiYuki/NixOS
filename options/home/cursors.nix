1d4124a50b3f37567edf254f1f65daa07bc59464
Asaki Yuki <vantrong2007vn@gmail.com>
2026-09-09 14:29:11 +0700

    chore(options): idk

--- options/home/cursors.nix
+++ options/home/cursors.nix
@@ -10,7 +10,7 @@
 in {
   options = {
     theme.cursors = lib.mkOption {
-      type = lib.types.enum ([null] ++ (builtins.filter (name: !lib.hasPrefix "#" name) (lib.attrNames cursors)));
+      type = lib.types.enum ([null] ++ (lib.attrNames cursors));
       default = null;
       description = "Cursor theme to use for the desktop";
     };
