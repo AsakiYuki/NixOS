7fa0d283fd32ead6521333ac09a86734012e2ec7
Asaki Yuki <vantrong2007vn@gmail.com>
2026-09-10 21:59:15 +0700

    chore: add wakatime in global extensions

--- devices/home-server/programs/home/asakiyuki.nix
+++ devices/home-server/programs/home/asakiyuki.nix
@@ -1,8 +1,16 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  unstable-pkgs,
+  ...
+}: {
   programs = {
     cider = {
       enable = true;
       package = pkgs.cider-2;
     };
+
+    vscodium.global.extensions = with unstable-pkgs.vscode-extensions; [
+      wakatime
+    ];
   };
 }
